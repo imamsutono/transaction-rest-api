b5b07d86927b503868889d66f26c059681697498
Stephen Daniel <stephendaniel60@gmail.com>
2021-04-24 21:45:18 +0700

    Done create transaction API

---
+++ api/create.php
@@ -0,0 +1,30 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+	header("Content-Type: application/json; charset=UTF-8");
+	header("Access-Control-Allow-Method: POST");
+	header("Access-Control-Max-Age: 3600");
+	header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+	include_once '../config/database.php';
+	include_once '../class/transaction.php';
+
+	$database = new Database();
+	$db = $database->getConnection();
+
+	$item = new Transaction($db);
+
+	$data = json_decode(file_get_contents("php://input"));
+
+	$item->invoice_id = $data->invoice_id;
+	$item->merchant_id = $data->merchant_id;
+	$item->item_name = $data->item_name;
+	$item->amount = $data->amount;
+	$item->payment_type = $data->payment_type;
+	$item->number_va = $data->number_va;
+	$item->customer_name = $data->customer_name;
+
+	if ($item->createTransaction()) {
+		echo "Transaction created successfully";
+	} else {
+		echo "Transaction could not be created";
+	}
--- class/transaction.php
+++ class/transaction.php
@@ -21,7 +21,38 @@
 		}
 
 		public function createTransaction() {
-			
+			$query = "INSERT INTO ". $this->db_table ."
+					  SET
+					  invoice_id = :invoice_id,
+					  merchant_id = :merchant_id,
+					  item_name = :item_name,
+					  amount = :amount,
+					  payment_type = :payment_type,
+					  number_va = :number_va,
+					  customer_name = :customer_name";
+
+			$stmt = $this->conn->prepare($query);
+
+			$this->invoice_id = htmlspecialchars(strip_tags($this->invoice_id));
+			$this->merchant_id = htmlspecialchars(strip_tags($this->merchant_id));
+			$this->item_name = htmlspecialchars(strip_tags($this->item_name));
+			$this->amount = htmlspecialchars(strip_tags($this->amount));
+			$this->payment_type = htmlspecialchars(strip_tags($this->payment_type));
+			$this->number_va = htmlspecialchars(strip_tags($this->number_va));
+			$this->customer_name = htmlspecialchars(strip_tags($this->customer_name));
+
+			$stmt->bindParam(":invoice_id", $this->invoice_id);
+			$stmt->bindParam(":merchant_id", $this->merchant_id);
+			$stmt->bindParam(":item_name", $this->item_name);
+			$stmt->bindParam(":amount", $this->amount);
+			$stmt->bindParam(":payment_type", $this->payment_type);
+			$stmt->bindParam(":number_va", $this->number_va);
+			$stmt->bindParam(":customer_name", $this->customer_name);
+
+			if ($stmt->execute()) {
+				return true;
+			}
+			return false;
 		}
 
 		public function getStatus() {
